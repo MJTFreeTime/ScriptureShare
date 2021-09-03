dcbd32a15b45aef417ab66dacedcb8dd8282c686
Matthew T <mjtcraft@gmail.com>
2021-09-03 13:19:57 -0700

    Redesigned main page; added universal code; styled upload page a bit; re-did nav bar and made it show on all pages

---
+++ public/universal_code/universal.js
@@ -0,0 +1,10 @@
+document.write('<div class="top_nav"> \
+    <ul> \
+        <li><a href="../">Home</a></li> \
+        <li><a href="../gallery">Gallery</a></li> \
+        <li><a href="../upload">Upload</a></li> \
+        <li><a href="#">About Us</a></li> \
+        <li style="float:right"><a class ="cta" href="#">Login</a></li> \
+    </ul> \
+</div> \
+');
