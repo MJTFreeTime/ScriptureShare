0c51ac714a588f7a54d02153cf8dacc65da8f0ae
mjt-21 <mjtcraft@gmail.com>
2022-01-17 10:17:30 -0800

    Gallery Design Change

--- public/script.js
+++ public/script.js
@@ -1,13 +1,46 @@
 function addImage(src, toName) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.alt = toName;
-    document.getElementById("images").insertAdjacentHTML("afterbegin", 
-    '<div class="img_box" id="g_photo"> \
-    <img src="' + src + 
-    '"/> \
-    </div>'
-    )
+	// Create document fragment to temporarily hold a virtual "fragment" of DOM manipulation
+	let tempFrag = document.createDocumentFragment();
+	
+	let img_container = document.createElement("div");
+	img_container.className = "img_container";
+
+	let img_item = document.createElement("div");
+	img_item.className = "img_item";
+	img_container.appendChild(img_item);
+
+	let anchor = document.createElement("a");
+	anchor.href = src;
+	anchor.target = "_blank"; // To open href in new tab
+	anchor.rel = "noopener noreferrer" // To address tabnabbing vulnerability
+	img_item.appendChild(anchor)
+
+	let img = document.createElement("img");
+	img.src = src;
+	anchor.appendChild(img);
+
+	let desc = document.createElement("div");
+	desc.className = "desc";
+	img_container.appendChild(desc);
+
+	let desc_text = document.createElement("p");
+	desc_text.className = "desc_text";
+	desc_text.innerText = "Uploaded By: Soobinator";
+	desc.appendChild(desc_text);
+	
+	// Append img_container div to tempFrag
+	tempFrag.appendChild(img_container);
+	
+	document.querySelector(".img_gallery").appendChild(tempFrag);
+	
+	async function readFromClipboard() {
+	  try {
+		const text = await navigator.clipboard.readText();
+		console.log(text);
+	  } catch (error) {
+		console.error(error);
+	  }
+	}
 }
 
 var imageNames;
